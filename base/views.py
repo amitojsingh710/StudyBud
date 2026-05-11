d92673bfd6a0b4d194aaf175de41be8b5d37d39e
amitojsingh710 <aarshami710@gmail.com>
2026-05-11 17:30:42 +0530

    Added custom topic creation

--- base/views.py
+++ base/views.py
@@ -112,15 +112,26 @@ def userProfile(request, pk):
 
 @login_required(login_url='login')
 def createRoom(request):
+    topics = Topic.objects.all()
     form = RoomForm()
+
     if request.method == 'POST':
-        form = RoomForm(request.POST)
-        if form.is_valid():
-            room = form.save(commit=False)
-            room.host = request.user
-            room.save()
-            return redirect('home')
-    context = {'form': form}
+        topic_name = request.POST.get('topic')
+
+        topic, created = Topic.objects.get_or_create(
+            name=topic_name
+        )
+
+        room = Room.objects.create(
+            host=request.user,
+            topic=topic,
+            name=request.POST.get('name'),
+            description=request.POST.get('description'),
+        )
+
+        return redirect('home')
+
+    context = {'form': form, 'topics': topics}
     return render(request, 'base/room_form.html', context)
 
 @login_required(login_url='login')
