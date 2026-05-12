5f71ef30be188104ff82c19aee89d31b012f3841
amitojsingh710 <aarshami710@gmail.com>
2026-05-12 16:50:38 +0530

    Updated templates

--- studybud/settings.py
+++ studybud/settings.py
@@ -1,3 +1,4 @@
+import dj_database_url
 import os
 """
 Django settings for studybud project.
@@ -79,10 +80,9 @@ WSGI_APPLICATION = 'studybud.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        os.environ.get('DATABASE_URL')
+    )
 }
 
 
